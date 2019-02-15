d7675359330d6340668d4a72a48e09c60995310c
rberriz <rberriz@icloud.com>
2019-02-14 23:45:03 -0500

    about us page

--- JS/index.js
+++ JS/index.js
@@ -17,3 +17,35 @@ closer.addEventListener("click", function() {
   opener.classList.toggle("menuClose");
   closer.classList.toggle("menuClose");
 });
+
+let links = document.querySelectorAll(".link");
+
+class Tab {
+  constructor(link) {
+    this.link = link;
+    this.link.addEventListener("click", () => {
+      this.linkClick();
+    });
+    this.linkData = this.link.dataset.tab;
+    this.tabContent = document.querySelector(
+      `.content[data-tab='${this.linkData}']`
+    );
+    this.tabContent = new Content(this.tabContent);
+  }
+  linkClick() {
+    this.tabContent.toggleContent();
+  }
+}
+
+class Content {
+  constructor(tabContent) {
+    this.tabContent = tabContent;
+  }
+  toggleContent() {
+    const currentTab = document.querySelector(".tabShow");
+    currentTab && currentTab.classList.toggle("tabShow");
+    this.tabContent.classList.toggle("tabShow");
+  }
+}
+
+links = Array.from(links).map(link => new Tab(link));
